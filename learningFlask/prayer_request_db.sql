acec9c9dd060a3da3060619e88c54b1fb4562be4
woody05 <34251111+woody05@users.noreply.github.com>
2021-08-16 16:29:42 -0400

    Update prayer_request_db.sql
    Updated SQL script to create DB

--- learningFlask/prayer_request_db.sql
+++ learningFlask/prayer_request_db.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.5.4-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
+-- Host:                         192.168.200.68
+-- Server version:               10.0.28-MariaDB-2+b1 - Raspbian testing-staging
+-- Server OS:                    debian-linux-gnueabihf
 -- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
@@ -21,7 +21,23 @@ CREATE TABLE IF NOT EXISTS `category` (
   `Category_id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`Category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table prayer_request.comments
+CREATE TABLE IF NOT EXISTS `comments` (
+  `Comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `User_id` int(11) NOT NULL,
+  `Prayer_request_id` int(11) NOT NULL,
+  `Comment` varchar(5000) NOT NULL DEFAULT '',
+  `Date_Added` datetime NOT NULL,
+  PRIMARY KEY (`Comment_id`),
+  KEY `FK_prayer_request_comments` (`Prayer_request_id`),
+  KEY `FK_users_comments` (`User_id`),
+  CONSTRAINT `FK_prayer_request_comments` FOREIGN KEY (`Prayer_request_id`) REFERENCES `prayer_request` (`Prayer_Request_id`),
+  CONSTRAINT `FK_users_comments` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
@@ -30,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `prayer_request` (
   `Prayer_Request_id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(22) NOT NULL DEFAULT '0',
   `Description` varchar(1000) DEFAULT NULL,
-  `IsAnswered` tinyint(1) NOT NULL DEFAULT 0,
+  `IsAnswered` tinyint(1) NOT NULL DEFAULT '0',
   `Add_By` varchar(50) DEFAULT NULL,
   `Category_id` int(11) DEFAULT NULL,
   `Date_Added` date NOT NULL,
@@ -38,7 +54,19 @@ CREATE TABLE IF NOT EXISTS `prayer_request` (
   PRIMARY KEY (`Prayer_Request_id`),
   KEY `FK_prayer_request_category` (`Category_id`),
   CONSTRAINT `FK_prayer_request_category` FOREIGN KEY (`Category_id`) REFERENCES `category` (`Category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table prayer_request.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `User_id` int(11) NOT NULL AUTO_INCREMENT,
+  `User_name` varchar(50) NOT NULL,
+  `Email_address` varchar(50) NOT NULL,
+  `First_name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`User_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
